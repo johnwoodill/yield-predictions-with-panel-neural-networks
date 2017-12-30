270657df22bd291833f8c26b766240b083d64422
johnwoodill <johnwoodill@gmail.com>
2017-12-29 14:21:07 -1000

    predictions

--- yield_regression.R
+++ yield_regression.R
@@ -10,13 +10,14 @@ tdat <- select(tdat,dday0_10, dday10_30, dday30, prec, prec_sq)
 
 mod <- panelNNET.default(y = regdat$corn_yield, 
                          X = data.frame(regdat$dday0_10, regdat$dday10_30, regdat$dday30, regdat$prec, regdat$prec_sq),
+                         fe_var = regdat$state,
                          hidden_units = 1, 
-                         fe_var = regdat$fips, 
-                         time_var = regdat$year,
                          param = data.frame(regdat$dday0_10, regdat$dday10_30, regdat$dday30, regdat$prec, regdat$prec_sq ))
 
 old.pdat <- predict(mod)
 head(old.pdat)
 
-pdat <- predict(mod, tdat, new.param = tdat)
+pdat <- predict(mod, 
+                newX = data.frame(tdat$dday0_10, tdat$dday10_30, tdat$dday30, tdat$prec, tdat$prec_sq), 
+                new.param = data.frame(tdat$dday0_10, tdat$dday10_30, tdat$dday30, tdat$prec, tdat$prec_sq))
 
